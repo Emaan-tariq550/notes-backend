2d77b0a697992894b991b8312220a0a7e15979a9
Emaan Tariq <emaantariq550@gmail.com>
2026-05-19 14:42:44 +0500

    CORS fix

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://your-frontend.vercel.app'],
+  origin: ['http://localhost:5173', 'https://backend-neon-eta.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
